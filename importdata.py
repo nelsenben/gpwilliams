d5742bd54192a1d67f728f6c42ca335a4936fca1
nelsenben <bjaminnelsen@gmail.com>
2016-09-23 09:03:00 -0600

    Parsing of single file

--- importdata.py
+++ importdata.py
@@ -1,11 +1,26 @@
 
 import numpy as np
+import os
+import datetime
+import astropy.time
 
-def importTemperatureFolder():
+def importTemperatureFolder(s_folderPath):
     pass
 
-def importTemperatureFile():
-    pass
+def importTemperatureFile(s_folderPath, s_fileName):
+    s_date = []
+    d_lowTemp = []
+    d_highTemp = []
+
+    s_filePath = os.path.join(s_folderPath, s_fileName)
+    f_data = file(s_filePath,'r')
+    for line in f_data:
+        columns = line.split(' ')
+        s_date.append(columns[0])
+        d_lowTemp.append(float(columns[1]))
+        d_highTemp.append(float(columns[2]))
+
+    print('Debug')
 
 def generatePseudoDates(i_startDate, i_dataLength):
     """
@@ -21,6 +36,9 @@ def generatePseudoDates(i_startDate, i_dataLength):
     ia_date = ia_date + ia_startDate
     return ia_date
 
+def convertJulian(s_date, s_format):
+    it_dateTimeObject = datetime.datetime.strptime(s_date, s_format)
+    time = astropy.time.Time(it_dateTimeObject)
+    i_julian = int(time.jd)
 
-
-print("hello world")
+    return i_julian
