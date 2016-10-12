44dc8ed9016a44204bc27c1436613d1e8e24c98a
nelsenben <bjaminnelsen@gmail.com>
2016-10-12 09:39:14 -0600

    Fixed Pickling functionality

--- mainRun.py
+++ mainRun.py
@@ -5,17 +5,17 @@ import pickle
 s_folderPath = 'Z:\\TimeHistory\\MatLabCodeFromWeb\\Ben_Code\\data2'
 s_fileName = 'abraham25n.dat'
 
-s_storage = 'Z:\\TimeHistory\\MatLabCodeFromWeb\\Ben_Code\\data2\\pickleddata'
+s_storage = 'Z:\\TimeHistory\\MatLabCodeFromWeb\\Ben_Code\\Pickles\\data20161012.p'
 
 dataimport = 1
-analyze = 1
+analyze = 0
 
 if dataimport == 1:
 # test = id.importTemperatureFile(s_folderPath, s_fileName)
     test1 = id.importTemperatureFolder(s_folderPath)
-    pickle.dump(test1, s_storage, 0)
+    pickle.dump(test1, open(s_storage,'wb'), 0)
 
 if analyze == 1:
-    test1 = pickle.load(s_storage)
+    test1 = pickle.load(open(s_storage, 'rb'))
     run = EMD.EMD()
     run.runEMD()
